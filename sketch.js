4154e1626b772ec1d27a9121507549ba710c5f89
Mohalian <74673775+Mohalian@users.noreply.github.com>
2020-12-29 16:56:49 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+function setup() {
+  createCanvas(1600, 400);
+
+  
+}
+function draw(){
+	textSize(15);
+	text("Recipe for chocolate chip cookie", 800,100);
+	textSize(12);
+	text("First, get all the ingredients required for your cookies like eggs, baking soda, baking powder, and chocolate chips. Second, now you need to mix all your ingredients in a bowl. Keep mixing till all your ingriedients are well mixed. Finally, cut your batter in circles and put it in the oven for 10 minutes",20,200);
+
+} 
